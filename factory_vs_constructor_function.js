95c05f3b0733c7222983456d7195916d6b347490
elabida <40395061+elabida@users.noreply.github.com>
2018-07-26 14:16:12 +0200

    Create factory_vs_constructor_function.js

---
+++ factory_vs_constructor_function.js
@@ -0,0 +1,29 @@
+function factoryF() {
+  
+  // the first method...
+  // returning an object, that s the Factory function
+
+  let po = 12;
+  
+  return {
+    pi : 19,
+    fi : function() {console.log('creat_f: this.pi: ' + this.pi);},
+    fo : function() {console.log('creat_f: po: ' + po);} //closure!
+  };
+}
+
+let fact_f = factoryF();
+fact_f.fi();
+fact_f.fo();
+
+function ConstructorF(){
+  
+  // the second method...
+  // calling the new op, that s the constructor function
+  
+  this.p = 17;
+  this.f = function() {console.log('const_f: this.p : ' + this.p);};
+}
+
+let cons_f = new ConstructorF();
+cons_f.f();
